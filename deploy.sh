def643af39807260e061dd1ab66cf509d97db4f1
tinaxd <31384595+tinaxd@users.noreply.github.com>
2021-06-25 01:17:51 +0900

    deploy

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -x
+npm run build && \
+cp dist/* ../tinaxcraft-deploy/ && \
+mv ../tinaxcraft-deploy/index.html ../tinaxcraft-deploy/home.html && \
+touch ../tinaxcraft-deploy/index.php && \
+echo '<?php include_once("home.html"); ?>' > ../tinaxcraft-deploy/index.php
