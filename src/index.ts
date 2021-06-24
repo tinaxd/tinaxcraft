c2067c66ca87f6fa716dbec22242812ae5952628
tinaxd <31384595+tinaxd@users.noreply.github.com>
2021-06-24 20:12:34 +0900

    second

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,16 @@ const MoveForwardBit = 1;
 const MoveBackBit = 1 << 1;
 const MoveLeftBit = 1 << 2;
 const MoveRightBit = 1 << 3;
+const MoveUpBit = 1 << 4;
+const MoveDownBit = 1 << 5;
 let currentMove = 0;
+let position = vec3.create();
 
 canvas.addEventListener('keydown', (event) => {
+    if (event.repeat) return false;
     switch (event.key) {
     case 'w':
+        //console.log("w start");
         currentMove |= MoveForwardBit;
         break;
     case 's':
@@ -36,25 +41,57 @@ canvas.addEventListener('keydown', (event) => {
     case 'd':
         currentMove |= MoveRightBit;
         break;
+    case ' ':
+        currentMove |= MoveUpBit;
+        break;
+    case 'Shift':
+        currentMove |= MoveDownBit;
+        break;
+    default:
+        return true;
     }
+    return false;
 });
 
 canvas.addEventListener('keyup', (event) => {
     switch (event.key) {
-        case 'w':
-            currentMove &= ~MoveForwardBit;
-            break;
-        case 's':
-            currentMove &= ~MoveBackBit;
-            break;
-        case 'a':
-            currentMove &= ~MoveLeftBit;
-            break;
-        case 'd':
-            currentMove &= ~MoveRightBit;
-            break;
-        }
+    case 'w':
+        //console.log("w end");
+        currentMove &= ~MoveForwardBit;
+        break;
+    case 's':
+        currentMove &= ~MoveBackBit;
+        break;
+    case 'a':
+        currentMove &= ~MoveLeftBit;
+        break;
+    case 'd':
+        currentMove &= ~MoveRightBit;
+        break;
+    case ' ':
+        currentMove &= ~MoveUpBit;
+        break;
+    case 'Shift':
+        currentMove &= ~MoveDownBit;
+        break;
+    default:
+        return true;
+    }
+    return false;
 });
+
+canvas.addEventListener('click', (event) => {
+    canvas.requestPointerLock();
+});
+
+let mouseMoveX = 0;
+let mouseMoveY = 0;
+
+canvas.addEventListener('mousemove', (event) => {
+    mouseMoveX += event.movementX;
+    mouseMoveY += event.movementY;
+    console.log(mouseMoveX);
+}, false);
 
 function genBlocksVertices(coords: Array<Array<number>>) {
     const base = [];
@@ -192,18 +229,83 @@ function initWebgl() {
 }
 
 function startRenderLoop() {
-    renderLoop(null);
+    lastRender = performance.now();
+    renderLoop(lastRender+1);
 }
 
-function renderLoop(now) {
+let lookAtVec = vec3.fromValues(1, 1, 0);
+let lastRender;
+const zUpVec = vec3.fromValues(0, 0, 1);
+
+let cameraAngleX = 0;
+let cameraAngleY = 0;
+
+function renderLoop(now: number) {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    if (now) {
+        const deltaMillis = now - lastRender;
+
+        // player camera move
+        const sensitivityX = -0.0007;
+        const sensitivityY = -0.0007;
+        cameraAngleX += mouseMoveX * sensitivityX;
+        cameraAngleY += mouseMoveY * sensitivityY;
+
+        if (cameraAngleX > Math.PI) {
+            cameraAngleX -= 2 * Math.PI;
+        } else if (cameraAngleX < Math.PI) {
+            cameraAngleX += 2 * Math.PI;
+        }
+        if (cameraAngleY > Math.PI / 2) {
+            cameraAngleY = Math.PI / 2;
+        } else if (cameraAngleY < -Math.PI / 2) {
+            cameraAngleY = -Math.PI / 2;
+        }
+
+        lookAtVec[0] = Math.cos(cameraAngleX) * Math.cos(cameraAngleY);
+        lookAtVec[1] = Math.sin(cameraAngleX) * Math.cos(cameraAngleY);
+        lookAtVec[2] = Math.sin(cameraAngleY);
+
+        mouseMoveX = 0;
+        mouseMoveY = 0;
+
+        // player position move
+        const move = currentMove;
+        vec3.normalize(lookAtVec, lookAtVec);
+        //console.log(lookAtVec);
+        if (move & MoveForwardBit) {
+            position[0] += lookAtVec[0] * deltaMillis / 1000;
+            position[1] += lookAtVec[1] * deltaMillis / 1000;
+        }
+        if (move & MoveBackBit) {
+            position[0] -= lookAtVec[0] * deltaMillis / 1000;
+            position[1] -= lookAtVec[1] * deltaMillis / 1000;
+        }
+        if (move & MoveLeftBit) {
+            position[0] -= lookAtVec[1] * deltaMillis / 1000;
+            position[1] += lookAtVec[0] * deltaMillis / 1000;
+        }
+        if (move & MoveRightBit) {
+            position[0] += lookAtVec[1] * deltaMillis / 1000;
+            position[1] -= lookAtVec[0] * deltaMillis / 1000;
+        }
+        if (move & MoveUpBit) {
+            position[2] += zUpVec[2] * deltaMillis / 1000;
+        }
+        if (move & MoveDownBit) {
+            position[2] -= zUpVec[2] * deltaMillis / 1000;
+        }
+        //console.log(position);
+    }
 
     const model = mat4.create();
 
     const view = mat4.create();
-    const eye = vec3.fromValues(-2, -1, 3);
-    mat4.lookAt(view, eye, vec3.create(), vec3.fromValues(0, 0, 1));
+    const lookTarget = vec3.create();
+    vec3.add(lookTarget, position, lookAtVec);
+    mat4.lookAt(view, position, lookTarget, zUpVec);
     
     const proj = mat4.create();
     mat4.perspective(proj, glMatrix.toRadian(75), 16/9, 0.1, 5);
@@ -218,5 +320,8 @@ function renderLoop(now) {
     gl.drawArrays(gl.TRIANGLES, 0, numberOfVertices);
     gl.flush();
 
+    lastRender = now;
+
     requestAnimationFrame(renderLoop);
+    
 }
