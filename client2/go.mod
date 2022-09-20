35587afe4dd619587b581795e4511493a041a586
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-09-20 22:02:52 +0900

    mod

--- client2/go.mod
+++ client2/go.mod
@@ -1,3 +1,8 @@
 module github.com/tinaxd/tinaxcraft/client2
 
 go 1.19
+
+require (
+	github.com/go-gl/gl v0.0.0-20211210172815-726fda9656d6
+	github.com/veandco/go-sdl2 v0.4.25
+)
---
+++ client2/main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/go-gl/gl/v4.1-core/gl"
+	"github.com/veandco/go-sdl2/sdl"
+)
+
+func init() {
+	runtime.LockOSThread()
+}
+
+func initMain() {
+	if err := sdl.Init(sdl.INIT_EVERYTHING); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+		800, 600, sdl.WINDOW_SHOWN|sdl.WINDOW_RESIZABLE|sdl.WINDOW_OPENGL)
+	if err != nil {
+		panic(err)
+	}
+	defer window.Destroy()
+
+	glCtx, err := window.GLCreateContext()
+	if err != nil {
+		panic(err)
+	}
+
+	// err = sdl.GLSetSwapInterval(1)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	if err := window.GLMakeCurrent(glCtx); err != nil {
+		panic(err)
+	}
+
+	if err := sdl.GLSetSwapInterval(1); err != nil {
+		panic(err)
+	}
+
+	if err := gl.Init(); err != nil {
+		panic(err)
+	}
+
+	gl.ClearColor(0, 0, 0, 1)
+	gl.Clear(gl.COLOR_BUFFER_BIT)
+	// window.GLSwap()
+
+	// sdl.GLDeleteContext(glCtx)
+}
+
+func main() {
+	initMain()
+
+	running := true
+	for running {
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch event.(type) {
+			case *sdl.QuitEvent:
+				println("Quit")
+				running = false
+				break
+			}
+		}
+	}
+}
