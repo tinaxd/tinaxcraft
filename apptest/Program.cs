283fdf7c7dde322f01421ba825088e5ff8350329
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-09-21 01:53:52 +0900

    c#test

---
+++ apptest/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tinax.Tinaxcraft
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
+            using (Game game = new Game(800, 600, "LearnOpenTK"))
+            {
+                //Run takes a double, which is how many frames per second it should strive to reach.
+                //You can leave that out and it'll just update as fast as the hardware will allow it.
+                game.Run();
+            }
+        }
+    }
+}
