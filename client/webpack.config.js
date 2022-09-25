7bcbaeabda2fb98537a4d73cd891070f8cc5e5e5
tinaxd <31384595+tinaxd@users.noreply.github.com>
2022-09-25 12:40:27 +0900

    updated everything

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const process = require('process');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const process = require("process");
 
 module.exports = {
-    mode: process.env.WEBPACK_MODE || 'development',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: '/node_modules/'
-            },
-            {
-                test: /\.glsl$/,
-                type: 'asset/source'
-            }
-        ]
-    },
-    plugins: [
-        new CopyPlugin({
-            patterns: [
-                {from: 'public', to: ''}
-            ]
-        })
+  mode: process.env.WEBPACK_MODE || "development",
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: "/node_modules/",
+      },
+      {
+        test: /\.glsl$/,
+        type: "asset/source",
+      },
     ],
-    devServer: {
-        contentBase: './dist'
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "" }],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
     },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js', '.glsl']
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".glsl"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 };
